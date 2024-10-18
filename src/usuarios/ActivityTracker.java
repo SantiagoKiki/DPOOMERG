6d363f1f0bcf8d198af42ded92af9153d16aee14
samuelmolinal <s.molinal@uniandes.edu.co>
2024-10-18 12:42:32 -0500

    avance clases progressTracker y activityTracker

---
+++ src/usuarios/ActivityTracker.java
@@ -0,0 +1,89 @@
+package usuarios;
+
+import java.util.Date;
+
+public class ActivityTracker<Activity> {
+	
+	
+	private Activity activity;
+	private int dedicatedTime;
+	private String status;
+	protected Date startDate;
+	protected Date completionDate;
+	
+	
+	public ActivityTracker(Activity activity, int dedicatedTime, String status, Date startDate, Date completionDate) {
+		this.activity = activity;
+		this.dedicatedTime = dedicatedTime;
+		this.status = status;
+		this.startDate = startDate;
+		this.completionDate = completionDate;
+	}
+	
+	
+	public Activity getActivity() {
+		return activity;
+	}
+
+
+	public void setActivity(Activity activity) {
+		this.activity = activity;
+	}
+
+
+	public int getDedicatedTime() {
+		return dedicatedTime;
+	}
+
+
+	public void setDedicatedTime(int dedicatedTime) {
+		this.dedicatedTime = dedicatedTime;
+	}
+
+
+	public String getStatus() {
+		return status;
+	}
+
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public Date getCompletionDate() {
+		return completionDate;
+	}
+
+
+	public void setCompletionDate(Date completionDate) {
+		this.completionDate = completionDate;
+	}
+
+
+	public float calculateProgress() {
+		if (this.status.equals("Completed")) {
+			return 100.0f;
+		}
+		float expectedDuration = activity.getExpectedDuration();
+		return (float)dedicatedTime /expectedDuration * 100;
+	}
+	
+	public void recordActivityCompletion() {
+		this.status = "Completed";
+		this.completionDate = new Date();
+	}
+	
+	
+
+}
---
+++ src/usuarios/ProgressTracker.java
@@ -0,0 +1,98 @@
+package usuarios;
+
+import java.util.Date;
+import java.util.List;
+
+public class ProgressTracker<LearningPath, ActivityTracker>{
+	
+	private String studentUsername;
+	private LearningPath learningpath;
+	private List<ActivityTracker> activitiestracker;
+	protected Date startDate;
+	protected Date completionDate;
+	
+	
+	public ProgressTracker(String studentUsername, LearningPath learningpath, List<ActivityTracker> activitiestracker,
+			Date startDate, Date completionDate) {
+		
+		this.studentUsername = studentUsername;
+		this.learningpath = learningpath;
+		this.activitiestracker = activitiestracker;
+		this.startDate = startDate;
+		this.completionDate = completionDate;
+	}
+
+
+	public String getStudentUsername() {
+		return studentUsername;
+	}
+
+
+	public void setStudentUsername(String studentUsername) {
+		this.studentUsername = studentUsername;
+	}
+
+
+	public LearningPath getLearningpath() {
+		return learningpath;
+	}
+
+
+	public void setLearningpath(LearningPath learningpath) {
+		this.learningpath = learningpath;
+	}
+
+
+	public List<ActivityTracker> getActivitiestracker() {
+		return activitiestracker;
+	}
+
+
+	public void setActivitiestracker(List<ActivityTracker> activitiestracker) {
+		this.activitiestracker = activitiestracker;
+	}
+
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+
+	public Date getCompletionDate() {
+		return completionDate;
+	}
+
+
+	public void setCompletionDate(Date completionDate) {
+		this.completionDate = completionDate;
+	}
+	
+	public float calculateProgress() {
+		int cont = 0;
+		if (this.activitiestracker.isEmpty()) {
+			return 0.0f;
+		}
+		for (ActivityTracker activitytracker: this.activitiestracker) {
+			if (activitytracker.getStatus().equals("Completed")) {
+				cont++;
+			}
+		}
+		return ((float) cont/this.activitiestracker.size()) * 100;
+	}
+	
+	// No se muy bien como implementar el metodo con respecto al uml
+	public void completeActivity() {
+		this.completionDate = new Date(); 
+	}
+	
+	
+	
+	
+	
+	
+}
