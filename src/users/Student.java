d00a7eb72bd2f93a9a4eef0530e1e6589a469f91
Kiki Santiago Quiroz <s.quirozp@uniandes.edu.co>
2024-10-17 20:05:25 -0500

    aa

---
+++ src/users/Student.java
@@ -0,0 +1,35 @@
+package users;
+
+import java.util.ArrayList;
+
+public class Student extends User {
+
+	private ArrayList<String>  interest = new ArrayList<String>();
+	private ArrayList <ProgressTracker> progressTracker= new ArrayList();
+	
+	
+	
+	private void addInteres(String interest){
+		
+	}
+	
+	private void remov
+	
+	
+	public ArrayList<String> getInterest() {
+		return interest;
+	}
+	public void setInterest(ArrayList<String> interest) {
+		this.interest = interest;
+	}
+	public ArrayList<ProgressTracker> getProgressTracker() {
+		return progressTracker;
+	}
+	public void setProgressTracker(ArrayList<ProgressTracker> progressTracker) {
+		this.progressTracker = progressTracker;
+	}
+	
+	
+	
+	
+}
